a695e3fcbc3ee321b0f6a60f09223fad04309346
NamJungsook <damlady@nate.com>
2016-10-12 13:56:36 +0900

    complite web

--- www/04/js/globalvars.js
+++ www/04/js/globalvars.js
@@ -5,25 +5,30 @@ var pages = [
 	{pg:1, url:"04_01.html", category:"준비하기", text:"INTRO"},
 	{pg:2, url:"04_02.html", category:"준비하기", text:"학습열기"},
 	{pg:3, url:"04_03.html", category:"준비하기", text:"학습목표"},
+
 	{pg:4, url:"04_04.html", category:"학습하기", text:"테이블 생성"},
 	{pg:5, url:"04_05.html", category:"학습하기", text:"테이블 생성"},
 	{pg:6, url:"04_06.html", category:"학습하기", text:"테이블 생성"},
 	{pg:7, url:"04_07.html", category:"학습하기", text:"테이블 생성"},
-	{pg:8, url:"04_08.html", category:"학습하기", text:"테이블 변경"},
-	{pg:9, url:"04_09.html", category:"학습하기", text:"테이블 변경"},
-	{pg:10, url:"04_10.html", category:"학습하기", text:"테이블 변경"},
-	{pg:11, url:"04_11.html", category:"학습하기", text:"테이블 변경"},
-	{pg:12, url:"04_12.html", category:"학습하기", text:"테이블 변경"},
-	{pg:13, url:"04_13.html", category:"평가하기", text:"적용하기"},
-	{pg:14, url:"04_14.html", category:"평가하기", text:"문제풀기"},
-	{pg:15, url:"04_15.html", category:"정리하기", text:"핵심요약"},
-	{pg:16, url:"04_16.html", category:"정리하기", text:"학습맺음"},
-	{pg:17, url:"04_17.html", category:"정리하기", text:"학습맺음"},
-	{pg:18, url:"04_18.html", category:"정리하기", text:"학습맺음"},
-	{pg:19, url:"04_19.html", category:"정리하기", text:"학습맺음"},
-	{pg:20, url:"04_20.html", category:"정리하기", text:"학습맺음"},
-	{pg:21, url:"04_21.html", category:"정리하기", text:"학습맺음"}
+	{pg:8, url:"04_08.html", category:"학습하기", text:"테이블 생성"},
+	{pg:9, url:"04_09.html", category:"학습하기", text:"테이블 생성"},
+	{pg:10, url:"04_10.html", category:"학습하기", text:"테이블 생성"},
+	{pg:11, url:"04_11.html", category:"학습하기", text:"테이블 생성"},
+	{pg:12, url:"04_12.html", category:"학습하기", text:"테이블 생성"},
 
+
+	{pg:13, url:"04_13.html", category:"학습하기", text:"테이블 변경"},
+	{pg:14, url:"04_14.html", category:"학습하기", text:"테이블 변경"},
+	{pg:15, url:"04_15.html", category:"학습하기", text:"테이블 변경"},
+	{pg:16, url:"04_16.html", category:"학습하기", text:"테이블 변경"},
+	{pg:17, url:"04_17.html", category:"학습하기", text:"테이블 변경"},
+
+
+
+	{pg:18, url:"04_18.html", category:"평가하기", text:"적용하기"},
+	{pg:19, url:"04_19.html", category:"평가하기", text:"문제풀기"},
+	{pg:20, url:"04_20.html", category:"정리하기", text:"핵심요약"},
+	{pg:21, url:"04_21.html", category:"정리하기", text:"학습맺음"}
 ];
 
 var totalPage = pages.length;
