62d6439e59621e3d7ae5494ad186037bcf269c17
NamJungsook <damlady@nate.com>
2016-12-06 00:22:14 +0900

    aa

---
+++ www/06/js/globalvars.js
@@ -0,0 +1,31 @@
+
+
+
+
+var totalPage = 16;
+
+var subject = "빅데이터 분석 결과 시각화";
+var subtitle = "영화 리뷰 분석 시각화";
+
+var pages = [
+	{pg:1, url:"10_01.html", category:"준비하기", text:"INTRO"}, 
+	{pg:2, url:"10_02.html", category:"준비하기", text:"학습열기"}, 
+	{pg:3, url:"10_03.html", category:"준비하기", text:"학습목표"}, 
+	{pg:4, url:"10_04.html", category:"학습하기", text:"비정형 데이터"},
+	{pg:5, url:"10_05.html", category:"학습하기", text:"비정형 데이터"},
+	{pg:6, url:"10_06.html", category:"학습하기", text:"비정형 데이터"}, 
+	{pg:7, url:"10_07.html", category:"학습하기", text:"'N사' 영화 리뷰 분석"}, 
+	{pg:8, url:"10_08.html", category:"학습하기", text:"'N사' 영화 리뷰 분석"}, 
+	{pg:9, url:"10_09.html", category:"학습하기", text:"'N사' 영화 리뷰 분석"}, 
+	{pg:10, url:"10_10.html", category:"학습하기", text:"'D사' 영화 리뷰 분석"}, 
+	{pg:11, url:"10_11.html", category:"학습하기", text:"'D사' 영화 리뷰 분석"}, 
+	{pg:12, url:"10_12.html", category:"학습하기", text:"'D사' 영화 리뷰 분석"}, 
+	{pg:13, url:"10_13.html", category:"평가하기", text:"적용하기"}, 
+	{pg:14, url:"10_14.html", category:"평가하기", text:"문제풀기"}, 
+	{pg:15, url:"10_15.html", category:"정리하기", text:"핵심요약"}, 
+	{pg:16, url:"10_16.html", category:"정리하기", text:"학습맺음"}, 
+];
+
+
+
+
---
+++ www/06/js/outro.js
@@ -0,0 +1,58 @@
+var OutroMotion = (function(){
+	var loopCount = 0;
+
+	var self = {
+		init:function(){
+			self.setInMotion();
+			self.startInMotion();
+			self.startLoopMotion();
+		},
+
+
+		setInMotion:function(){
+			TweenMax.to($(".img-01"), 0, {x:0, autoAlpha:0});
+			TweenMax.to($(".img-02"), 0, {x:0, autoAlpha:0});
+			TweenMax.to($(".data-box"), 0, {x:0, autoAlpha:0});
+
+			TweenMax.to($(".data-name"), 0, {x:120, autoAlpha:0});
+			TweenMax.to($(".subject-01"), 0, {y:20, autoAlpha:0});
+			TweenMax.to($(".data-01"), 0, {y:20, autoAlpha:0});
+			TweenMax.to($(".data-02"), 0, {y:20, autoAlpha:0});
+			TweenMax.to($(".data-03"), 0, {y:20, autoAlpha:0});
+			TweenMax.to($(".data-04"), 0, {y:20, autoAlpha:0});
+			TweenMax.to($(".data-05"), 0, {y:20, autoAlpha:0});
+
+			TweenMax.to($(".course-name"), 0, {x:0, autoAlpha:0});
+			TweenMax.to($(".chapter-name"), 0, {x:-120, autoAlpha:0});
+			TweenMax.to($(".ending-text"), 0, {x:-120, autoAlpha:0});
+			TweenMax.to($(".page-ending"), 0, {x:0, autoAlpha:0});
+		},
+
+
+		startInMotion:function(){
+			TweenMax.to($(".img-01"), 1, {delay:3, x:0, autoAlpha:1, ease:Cubic.easeOut});
+			TweenMax.to($(".img-02"), 1, {delay:1.5, x:0, autoAlpha:1, ease:Cubic.easeOut});
+			TweenMax.to($(".data-box"), 1, {delay:0.5, x:0, autoAlpha:1, ease:Cubic.easeOut});
+
+			TweenMax.to($(".data-name"), 0.5, {delay:3, x:0, autoAlpha:1, ease:Cubic.easeOut});
+			TweenMax.to($(".subject-01"), 1, {delay:3, y:0, autoAlpha:1, ease:Cubic.easeOut});
+			TweenMax.to($(".data-01"), 1, {delay:3.3, y:0, autoAlpha:1, ease:Cubic.easeOut});
+			TweenMax.to($(".data-02"), 1, {delay:3.6, y:0, autoAlpha:1, ease:Cubic.easeOut});
+			TweenMax.to($(".data-03"), 1, {delay:3.9, y:0, autoAlpha:1, ease:Cubic.easeOut});
+			TweenMax.to($(".data-04"), 1, {delay:4.2, y:0, autoAlpha:1, ease:Cubic.easeOut});
+			TweenMax.to($(".data-05"), 1, {delay:4.5, y:0, autoAlpha:1, ease:Cubic.easeOut});
+
+			TweenMax.to($(".course-name"), 1.5, {delay:1, x:0, autoAlpha:1, ease:Cubic.easeOut});
+			TweenMax.to($(".chapter-name"),1, {delay:1.5, x:0, autoAlpha:1, ease:Cubic.easeOut});
+			TweenMax.to($(".ending-text"),1, {delay:2, x:0, autoAlpha:1, ease:Cubic.easeOut});
+			TweenMax.to($(".page-ending"),4, {delay:4, x:0, autoAlpha:1, ease:Cubic.easeOut});
+		}
+	}
+
+	return self;
+})();
+
+$(function(){
+	OutroMotion.init();
+})
+
